d3597bdba2504d59ad05b14ddc10e2872a1841be
Lili <lilidl.nft@gmail.com>
2023-05-28 20:32:45 -0300

    add html index and fastapi-chameleon

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import fastapi
+import fastapi_chameleon
+from fastapi_chameleon import template
 import uvicorn
 
 app = fastapi.FastAPI()
 
-@app.get("/")
-def index():
-    return "Test"
+fastapi_chameleon.global_init("C:\Users\licdi\Desktop\Python\pypi_clone_fastapi\pypi_clone_fastapi\templates")
 
-uvicorn.run(app)
+@app.get("/")
+@template(template_file="index.html")
+
+def index():
+    return {
+        
+        "user_name": "Lili" 
+        
+        }
+
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
